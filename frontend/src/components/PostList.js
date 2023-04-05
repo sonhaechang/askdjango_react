bf241502835af8eccfceaeaab9760efc0efef035
Son Haechang <sonhaechang@Sonui-MacBookPro.local>
2023-04-06 00:42:13 +0900

    refector: 12 - 포스팅 목록 API에 JWT 인증 적용하기

--- frontend/src/components/PostList.js
+++ frontend/src/components/PostList.js
@@ -1,13 +1,21 @@
 import React, { useEffect, useState } from 'react';
+import { Alert } from "antd";
 import Axios from 'axios';
 import Post from 'components/Post';
+import { useAppContext } from 'store';
 
 function PostList() {
-    const apiUrl = 'http://127.0.0.1:8000/api/posts';
-    const [postList, setPostList] = useState([]);
+    const apiUrl = 'http://127.0.0.1:8000/api/posts/';
+    const [postList, setPostList] = useState([]); 
+    const {
+        store: { accessToken },
+        dispatch
+    } = useAppContext();
 
     useEffect(() => {
-        Axios.get(apiUrl)
+        const headers = { Authorization: `Bearer ${accessToken}` }
+
+        Axios.get(apiUrl, { headers })
             .then(response => {
                 const { data } = response;
                 setPostList(data);
@@ -19,6 +27,10 @@ function PostList() {
 
     return (
         <div>
+            {postList.length === 0 && (
+                <Alert type="warning" message="포스팅이 없습니다. :-(" />
+            )}
+
             {postList.map(post =>
                 <Post post={post} key={post.id} />
             )}
