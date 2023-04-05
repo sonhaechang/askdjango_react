1b301b5e9fc9c52c68d17b55676fc8e1b5597700
Son Haechang <sonhaechang@Sonui-MacBookPro.local>
2023-04-05 20:45:32 +0900

    feat: 09 - 로그인 폼을 만들고 로컬스토리지에 jwt 저장하기

--- frontend/src/pages/accounts/Login.js
+++ frontend/src/pages/accounts/Login.js
@@ -1,9 +1,122 @@
-import React from 'react';
+import React, { useState } from "react";
+import { Card, Form, Input, Button, notification } from "antd";
+import { SmileOutlined, FrownOutlined } from "@ant-design/icons";
+import { useNavigate } from "react-router-dom";
+import Axios from "axios";
+import useLocalStorage from "utils/useLocalStorage";
 
 function Login() {
+    const navigate = useNavigate();
+    const [fieldErrors, setFieldErrors] = useState({});
+    const [accessToken, setAccessToken] = useLocalStorage('accessToken', '');
+    const [refreshToken, setRefreshToken] = useLocalStorage('refreshToken', '');
+
+    const layout = {
+        labelCol: { span: 8 },
+        wrapperCol: { span: 16 }
+    };
+      
+    const tailLayout = {
+        wrapperCol: { offset: 8, span: 16 }
+    };
+
+    const onFinish = values => {
+        async function fn() {
+            setFieldErrors({});
+
+            const { username, password } = values;
+            const data = { username, password };
+            try {
+                    const response = await Axios.post(
+                        "http://localhost:8000/accounts/token/",
+                        data
+                    );
+                    
+                    const {
+                        data: { 
+                            access: accessToken,
+                            refresh: refreshToken
+                        }
+                    } = response;
+            
+                    setAccessToken(accessToken);
+                    setRefreshToken(refreshToken);
+            
+                    notification.open({
+                        message: "로그인 성공",
+                        icon: <SmileOutlined style={{ color: "#108ee9" }} />
+                    });
+        
+                // navigate('/accounts/login');  // TODO: 이동 주소
+                } catch (error) {
+                    if (error.response) {
+                        notification.open({
+                            message: "로그인 실패",
+                            description: "아이디/암호를 확인해주세요.",
+                            icon: <FrownOutlined style={{ color: "#ff3333" }} />
+                        });
+        
+                        const { data: fieldsErrorMessages } = error.response;
+                        // fieldsErrorMessages => { username: "m1 m2", password: [] }
+                        // python: mydict.items()
+                        setFieldErrors(
+                            Object.entries(fieldsErrorMessages).reduce(
+                                (acc, [fieldName, errors]) => {
+                                    // errors : ["m1", "m2"].join(" ") => "m1 "m2"
+                                    acc[fieldName] = {
+                                        validateStatus: "error",
+                                        help: errors.join(" ")
+                                    };
+                                    return acc;
+                                },
+                                {}
+                            )
+                        );
+                    }
+                }
+            }
+        fn();
+    };
+
     return (
-        <div>accounts/login</div>
-    )
-}
+        <Card title="로그인">
+            <Form
+                {...layout}
+                onFinish={onFinish}
+                //   onFinishFailed={onFinishFailed}
+                autoComplete={"false"}
+            >
+                <Form.Item
+                    label="Username"
+                    name="username"
+                    rules={[
+                        { required: true, message: "Please input your username!" },
+                        { min: 5, message: "5글자 입력해주세요." }
+                    ]}
+                    hasFeedback
+                    {...fieldErrors.username}
+                    {...fieldErrors.non_field_errors}
+                >
+                    <Input />
+                </Form.Item>
+        
+                <Form.Item
+                    label="Password"
+                    name="password"
+                    rules={[{ required: true, message: "Please input your password!" }]}
+                    {...fieldErrors.password}
+                >
+                    <Input.Password />
+                </Form.Item>
+        
+                <Form.Item {...tailLayout}>
+                    <Button type="primary" htmlType="submit">
+                        Submit
+                    </Button>
+                </Form.Item>
+            </Form>
+        </Card>
+    );
+};
 
 export default Login;
