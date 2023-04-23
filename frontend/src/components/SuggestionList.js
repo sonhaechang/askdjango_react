6965ea82a1e7853298d8e81f8332a364737ec5b5
Son Haechang <sonhaechang@Sonui-MacBookPro.local>
2023-04-24 01:08:23 +0900

    refector: userAxios 훅을 통한 보다 손쉬운 조회 API 요청

--- frontend/src/components/SuggestionList.js
+++ frontend/src/components/SuggestionList.js
@@ -1,34 +1,26 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Card } from "antd";
 import "./SuggestionList.scss";
 import Suggestion from "./Suggestion";
-import Axios from "axios";
+import useAxios from "axios-hooks"
 import { useAppContext } from 'store';
 
 function SuggestionList({ style }) {
-    const  [userList, setUserList] = useState([]);
     const { store: { accessToken } } = useAppContext();
-
-    useEffect(()=> {
-
-        const fetchUserList = async () => {
-            try {
-                const apiUrl = "http://localhost:8000/accounts/suggestions/";
-                const headers = { Authorization: `Bearer ${accessToken}` };
-                const { data } = await Axios.get(apiUrl, { headers });
-                setUserList(data);
-            } catch (error) {
-                console.error(error);
-            }
-        };
-
-        fetchUserList();
-    }, [setUserList, accessToken]);
+    const apiUrl = 'http://localhost:8000/accounts/suggestions/';
+    const headers = { Authorization: `Bearer ${accessToken}` };
+    // const [{ data: userList, loading, error }, refetch] = useAxios({
+    const [{ data: userList, loading, error }] = useAxios({
+        url: apiUrl, headers
+    });
 
     return (
         <div style={style}>
+            {loading && <div>Loading...</div>}
+            {error && <div>로딩중 에러가 발생했습니다.</div>}
+
             <Card title="Suggestions for you " size="small">
-                {userList.map(suggestionUser => (
+                {userList && userList.map(suggestionUser => (
                     <Suggestion
                         key={suggestionUser.username}
                         suggestionUser={suggestionUser}
